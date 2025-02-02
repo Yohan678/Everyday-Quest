cc36d6833b0ce7d2d814afc47cd19be858f458ba
Yohan678 <beginnerdevyohan@gmail.com>
2025-02-01 20:53:15 -0500

    Public_Variable_1

---
+++ Everyday Quest/Views/QuestView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestView.swift
+//  Everyday Quest
+//
+//  Created by Yohan Yoon on 2/1/25.
+//
+
+import SwiftUI
+
+struct QuestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestView()
+}
---
+++ Everyday Quest/Views/SharedVariable.swift
@@ -0,0 +1,8 @@
+//
+//  SharedVariable.swift
+//  Everyday Quest
+//
+//  Created by Yohan Yoon on 2/1/25.
+//
+
+import Foundation
